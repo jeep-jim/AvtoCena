89c6ebaf2a05ca48e83d2452e0cba987e5ecfe08
jeep-jim <yokkey@ya.ru>
2026-07-18 00:25:29 +0700

    Load targeted public price sheet fixes

--- apps/web/app/(public)/layout.tsx
+++ apps/web/app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 import "../flat-ui.css";
 import "../public-regression-fixes.css";
+import "../public-price-sheet-fix.css";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return children;
