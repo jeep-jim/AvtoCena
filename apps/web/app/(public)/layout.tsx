6d1049abf1aab4b0832b1b38364a6ca99afb7f07
jeep-jim <yokkey@ya.ru>
2026-08-31 09:55:23 +0700

    feat(analytics): install Yandex Metrika on public pages

--- apps/web/app/(public)/layout.tsx
+++ apps/web/app/(public)/layout.tsx
@@ -3,6 +3,18 @@ import "../public-regression-fixes.css";
 import "../public-price-sheet-fix.css";
 import "../catalog-filter-compact.css";
 import { CatalogFilterUiEnhancer } from "@/components/catalog/CatalogFilterUiEnhancer";
+import { YandexMetrikaRouteTracker } from "@/components/analytics/YandexMetrikaRouteTracker";
+
+const yandexMetrikaCounter = `
+(function(m,e,t,r,i,k,a){
+    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=112098062', 'ym');
+
+ym(112098062, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+`;
 
 const priceTrendTapGuard = `
 (() => {
@@ -287,8 +299,15 @@ html[data-theme="light"] .ac-public-footer-dealers:focus-visible {
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <script data-yandex-metrika="112098062" dangerouslySetInnerHTML={{ __html: yandexMetrikaCounter }} />
+      <noscript>
+        <div>
+          <img src="https://mc.yandex.ru/watch/112098062" style={{ position: "absolute", left: -9999 }} alt="" />
+        </div>
+      </noscript>
       {children}
       <CatalogFilterUiEnhancer />
+      <YandexMetrikaRouteTracker />
       <style dangerouslySetInnerHTML={{ __html: publicPageFixes }} />
       <script dangerouslySetInnerHTML={{ __html: priceTrendTapGuard }} />
     </>
